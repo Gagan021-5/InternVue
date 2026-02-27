6989045eee2ebc565ea8f49233d673e1941d8c51
Gagan021-5 <gagansingh010221@gmail.com>
2026-02-28 01:19:29 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000","https://internvue.onrender.com"],
     credentials: true,
   })
 );
